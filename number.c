178983265f34aeb656963386c0e67d42eec2c529
Lwxian27 <102198047+Lwxian27@users.noreply.github.com>
2022-03-23 14:07:47 +0800

    Update number.c

--- number.c
+++ number.c
@@ -3,7 +3,8 @@
 int main(int argc, const char *argv[])
 {
 	printf("hello world\n");
-	printf("aaaaaaaaaa");
-	printf("hello ok");
+	printf("aaaaaaaaaa\n");
+	printf("hello ok\n");
+	scanf("请输入---------");
 	return 0;
 }
